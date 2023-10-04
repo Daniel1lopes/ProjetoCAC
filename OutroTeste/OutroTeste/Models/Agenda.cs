8391299f89a9afb5a9a7af0e2d607d8438c1207f
Thurdias <thurdias@gmail.com>
2023-10-04 11:33:40 -0300

    adicionando arquivos branch arthur

---
+++ OutroTeste/OutroTeste/Models/Agenda.cs
@@ -0,0 +1,28 @@
+﻿using OutroTeste.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OutroTeste.Models
+{
+    [Table("Agenda", Schema = "CACTB")]
+
+    public class Agenda
+    {
+        [Key]
+        public int idAgenda { get; set; }
+        public DateTime dtAgenda { get; set; }
+        public TimeSpan hrInicio { get; set; }
+        public TimeSpan hrFim { get; set; }
+        public int nuVagas { get; set; }
+        public int nuReserva { get; set; }
+        public int nuAgendamentos { get; set; }
+        public short idServico { get; set; } 
+        public short idUnidadeAtendimento { get; set; } 
+
+        [ForeignKey("idServico, idUnidadeAtendimento")] 
+        public ServicoUnidadeAtendimento servicoUnidadeAtendimento { get; set; }
+
+    }
+}
---
+++ OutroTeste/OutroTeste/Models/Agendamento.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using OutroTeste.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OutroTeste.Models
+{
+    [Table("Agendamento", Schema = "CACTB")]
+    public class Agendamento
+    {
+        [Key]
+        public int idAgendamento { get; set; }
+        public DateTime dtAgendamento { get; set; }
+        public int idAgenda { get; set; }
+        public int idPessoa { get; set; }
+        [Column("icAtivo", TypeName = "bit")]
+        public bool icAtivo { get; set; }
+    }
+}
